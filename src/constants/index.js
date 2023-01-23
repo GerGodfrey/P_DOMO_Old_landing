9d580b7d14f38950099b966a91b3760360f69afd
GerGodfrey <luis-godfrey@hotmail.com>
2023-01-23 13:39:01 -0600

    nuevo url insta

--- src/constants/index.js
+++ src/constants/index.js
@@ -102,7 +102,7 @@ export const socialMedia = [
     {
       id: "social-media-1",
       icon: instagram,
-      link: "https://www.instagram.com/",
+      link: "https://www.instagram.com/domo_project/?hl=en",
     },
     {
       id: "social-media-3",
