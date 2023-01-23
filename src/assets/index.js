486c5402db53b4ae33597f8503fbef980a38787c
GerGodfrey <luis-godfrey@hotmail.com>
2023-01-23 13:50:21 -0600

    imagenes

---
+++ src/assets/index.js
@@ -0,0 +1,47 @@
+
+import close from "./close.svg";
+import logo_color from "./logo_color.svg";
+import logo_white from "./logo_white.svg";
+import menu from "./menu.svg";
+import NFTCollection from "./NFTCollection.png";
+import discount from "./Discount.svg";
+import mirrow from "./mirrow.svg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import card1 from "./card1.png";
+import card2 from "./Card2.png";
+import card3 from "./Card3.png";
+import card4 from "./Card4.png";
+import twitter_icon from "./twitter_icon.svg";
+import link_icon from "./link_icon.svg";
+import insta_icon from "./insta_icon.svg";
+import nft2 from "./nft.svg"
+import instagram from "./instagram.svg"
+import twitter from "./twitter.svg"
+import linkedin from "./linkedin.svg"
+
+
+export {
+    close,
+    logo_color,
+    menu,
+    NFTCollection,
+    discount,
+    mirrow,
+    send,
+    shield,
+    star,
+    card1,
+    card2,
+    card3,
+    card4,
+    twitter_icon,
+    link_icon,
+    insta_icon,
+    nft2,
+    logo_white,
+    instagram,
+    twitter,
+    linkedin
+};
