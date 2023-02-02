7fcba7d8af823a97bf0b26730cd722e417f15cd5
José de Jesús Acevedo Flores <jjaf@MacBook-Air-de-Jose-2.local>
2023-02-02 14:40:14 -0600

    Flex register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import styles from '../style';
-import { layout } from '../style';
+
 
 const Register = ({ showModal, setShowModal }) => {
 
@@ -27,7 +27,7 @@ const Register = ({ showModal, setShowModal }) => {
             {showModal ? (
                 <>
                 <div className='fixed z-50 inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
-                    <div className='w-[600px] px-10'>
+                    <div className={`${styles.boxWidth} max-w-3xl px-10`}>
                         <div className='bg-black border-t border-b border-solid border-l border-r rounded-lg'>
                             <div className="flex items-start justify-between bg-transparent p-10 border-b border-solid border-slate-200 rounded-t">
                                 <h3 className="text-3xl font-semibold text-gradient">
@@ -46,17 +46,19 @@ const Register = ({ showModal, setShowModal }) => {
                             <div className="p-6 flex-auto">
                                 <form className='flex-auto justify-center' onSubmit={submit} >
                                     <div className={`${styles.flexCenter}`}>
-                                        <label className='p-6 text-white font-semibold w-40' htmlFor="name">Nombre</label>
-                                        <input className='bg-black p-2 text-white
-                                        border-t border-b border-solid border-l border-r rounded-lg w-60' type="text" id='name' value={name} onChange={e => setName(e.target.value)} placeholder="Nombre" />
+                                        <label className={`p-6 text-white font-semibold ${styles.boxWidth}`} htmlFor="name">Nombre</label>
+                                        <input className={`bg-black p-3 text-white
+                                        border-t border-b border-solid border-l border-r rounded-lg ${styles.boxWidth}`} type="text" id='name' value={name} onChange={e => setName(e.target.value)} placeholder="Nombre" />
                                     </div>
                                     <div className={`${styles.flexCenter}`}>
-                                        <label className='p-6 text-white font-semibold w-40' htmlFor="age">Edad</label>
-                                        <input className='bg-black p-2 text-white border-t border-b border-solid border-l border-r rounded-lg w-60' type="number" id='age' value={age} onChange={e => setAge(e.target.value)} placeholder="Edad" />
+                                        <label className={`p-6 text-white font-semibold ${styles.boxWidth}`} htmlFor="age">Edad</label>
+                                        <input className={`bg-black p-3 text-white
+                                        border-t border-b border-solid border-l border-r rounded-lg ${styles.boxWidth}`}  type="number" id='age' value={age} onChange={e => setAge(e.target.value)} placeholder="Edad" />
                                     </div>
                                     <div className={`${styles.flexCenter}`}>
-                                        <label className='p-6 text-white font-semibold w-40' htmlFor="email">E-mail</label>
-                                        <input className='bg-black p-2 text-white border-t border-b border-solid border-l border-r rounded-lg w-60' type="text" id='email' value={email} onChange={e => setEmail(e.target.value)} placeholder="ejemplo@correo.com"/>
+                                        <label className={`p-6 text-white font-semibold ${styles.boxWidth}`} htmlFor="email">E-mail</label>
+                                        <input className={`bg-black p-3 text-white
+                                        border-t border-b border-solid border-l border-r rounded-lg ${styles.boxWidth}`}  type="text" id='email' value={email} onChange={e => setEmail(e.target.value)} placeholder="ejemplo@correo.com"/>
                                     </div>
                                 </form>
                             </div>
