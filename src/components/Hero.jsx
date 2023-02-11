6ec8597b680afb0a95d80fcb76a6e251592c36b5
GerGodfrey <luis-godfrey@hotmail.com>
2023-02-11 14:00:55 -0600

    cambios

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
                 <div className="flex flex-row justify-between items-center w-full">
                     <h1 className="flex-1 font-dmSans font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
                         <span className="text-gradient ">
-                            Lo bueno, bonito y barato <br className='sm:block hidden' /> {" "}
+                            Bueno, bonito y barato <br className='sm:block hidden' /> {" "}
                         </span>
                         INVIERTE EN EL MERCADO INMOBILIARIO
                     </h1>
